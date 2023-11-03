cc31d86ee8c692c0754db5fb3d26b39bf61392b0
GAURAV SHARMA <gsharma1154@gmail.com>
2023-11-03 16:20:23 +0000

    var ,let const, console.table,undefined value

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+let name="gaurav";
+const accNumber=444;//can not be changed
+var salary=4321234;//not used 
+let accState;//when  u dont initialize any variable in js ,by default it will give undefined value
+//var is not used in javascript as it gives error while working with block scope and function scope
+age=24;//when u cant define the data type of variables ,then also it does not gives error.
+//semi colon or not at the end wont matter
+name="feuncm";
+console.log(name);
+//accNumber=4;
+console.log(accNumber);
+//to show different data types in one table
+console.table([name,accNumber,age,salary,accState]);
