5c5758ce4c3753ced8ab0c10e29999fc1886f42a
GAURAV SHARMA <gsharma1154@gmail.com>
2023-11-05 21:51:12 +0000

    datatypes in js

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+//alert(3+3) //we are using node js not browser
+let name="Gaurav";
+let age=24;
+let salary=null;
+let state=true;
+//let can be used to denote every data type in js
+
+//different data types(primitive)
+//number-> 2 to the power 53
+//bigint->large integers (used in fb,insta,reddit etc)
+//string->""
+//boolean->false/true
+//null->stand alone value(agar koi server se data bheja hai aur reciever end pe value aaya nahi hai)
+//undefined->variable declare kara hai par value assign nahi ki hai 
+//symbol->used in react to find the uniqueness of components 
+
+//(non primitive data type)
+//object
+
+//typeof function
+console.log(typeof null);//object 
+console.log(typeof undefined);//undefined
