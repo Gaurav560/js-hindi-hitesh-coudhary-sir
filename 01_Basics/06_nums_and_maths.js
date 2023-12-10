3a95cdb614048873745646acb6b70366e6ab9a4c
GAURAV SHARMA <gsharma1154@gmail.com>
2023-12-10 21:29:36 +0000

    learned toFixed() and toPrecision()

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -1,5 +1,5 @@
 //it is type castingfrom string to number
-const score=400;
+const score=400.545343;
 console.log(score);
 
 
@@ -11,6 +11,16 @@ console.log(newNumber);
 console.log(typeof(score)); 
 console.log(typeof(newNumber));
 
-//converting the number into String
-newNumber.toString();
+//converting the number into String and calling length property to find the length of the string
+
+console.log(newNumber.toString().length);
+
+//applying toFixed() method used in finding precision of decimal numbers.toFixed(pass precision number) works directly on number and not on string
+//toFixed mein decimal ke baad vali ko count kar ke return karte hain String format mein
+console.log(newNumber.toFixed(7));
 console.log(typeof(newNumber))
+
+//using toPrecision method on a number.returns a string
+//toPrecision mein pele decimal ke left side aur jab left side mein khatm ho jaaye tab right side mein check hoti hai 
+console.log(newNumber.toPrecision(11));
+
