c47048b47c9442e3bb64bb7f4405514c21706c21
GAURAV SHARMA <gsharma1154@gmail.com>
2023-12-18 03:50:22 +0000

    dom manipultion

---
+++ 05_iterations/forEach.js
@@ -0,0 +1,6 @@
+//mainly used in array
+const myArrrrrr=[`raichan`,`pyschon`,`tyson`,`moti`,`motri`,`lottery`,`chapo chapo`];
+//forEach value dega usko use kar lo bhai 
+myArrrrrr.forEach((key)=>console.log(key));
+
+myArrrrrr.forEach(function(keyee){console.log(keyee);})
